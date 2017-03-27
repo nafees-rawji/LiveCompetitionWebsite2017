4e3166ed870bf8ce3220855a442220f788057737
Nafees Rawji <nafees.rawji@mail.utoronto.ca>
2017-03-27 00:21:15 -0400

    Adjusted offset in animation trigger

--- js/plugins.js
+++ js/plugins.js
@@ -418,10 +418,10 @@
         }
 
         var window_left = $window.scrollLeft();
-        var window_top = $window.scrollTop() + 50;
+        var window_top = $window.scrollTop() - 250;
         var offset = $element.offset();
         var left = offset.left;
-        var top = offset.top;
+        var top = (offset.top);
 
         if (top + $element.height() >= window_top &&
             top - ($element.data('appear-top-offset') || 0) <= window_top + $window.height() &&
