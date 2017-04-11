71ab9c15442e1959836d7500c800b5735b0d6eb8
Nafees Rawji <nafees.rawji@mail.utoronto.ca>
2017-04-10 21:52:04 -0400

    Added gallery to flc

--- js/flc.js
+++ js/flc.js
@@ -30,3 +30,25 @@ $('.previous-years-gallery').slick({
 });
 
 $('a').smoothScroll();
+
+if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    $('#gallery').html(`<h1 class="text-white text-center section-header">FLC 2015/2016 Gallery</h1>
+
+    <div id="nanogallery2" data-nanogallery2='{
+            "thumbnailHeight": 210, "thumbnailWidth": 210,
+            "thumbnailL1GutterWidth": 90,
+            "thumbnailL1GutterHeight": 90,
+            "itemsBaseURL": "img/flc/",
+            "thumbnailStacks": 4,
+            "thumbnailStacksTranslateZ": 0.3,
+            "thumbnailStacksRotateX": 0.9,
+            "galleryBuildInit2": "perspective_900px|perspective-origin_50% 150%"
+          }'>
+        <a href="flcphoto1.jpg" data-ngthumb="flcphoto1.jpg"></a>
+        <a href="flcphoto2.jpg" data-ngthumb="flcphoto2.jpg"></a>
+        <a href="flcphoto3.jpg" data-ngthumb="flcphoto3.jpg"></a>
+        <a href="flcphoto1.jpg" data-ngthumb="flcphoto4.jpg"></a>
+        <a href="flcphoto2.jpg" data-ngthumb="flcphoto5.jpg"></a>
+        <a href="flcphoto3.jpg" data-ngthumb="flcphoto6.jpg"></a>
+    </div>`)
+}
