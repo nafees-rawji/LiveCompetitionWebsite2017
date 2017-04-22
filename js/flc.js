f563ec54232b1fc2b78913a46c2556d0f57014e5
Nafees Rawji <nafees.rawji@mail.utoronto.ca>
2017-04-22 15:27:04 -0400

    Fixed index page

--- js/flc.js
+++ js/flc.js
@@ -18,6 +18,10 @@ $('#attend').appear().on('appear', function (event, $all_appeared_elements) {
     $("#attend").addClass("zoomIn");
 });
 
+$('#faq').appear().on('appear', function (event, $all_appeared_elements) {
+   $("#faq").addClass('zoomIn');
+});
+
 $.force_appear();
 
 $('.previous-years-gallery').slick({
@@ -30,31 +34,3 @@ $('.previous-years-gallery').slick({
 });
 
 $('a').smoothScroll();
-
-if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    $('#gallery').html(`<h1 class="text-white text-center section-header">FLC 2015/2016 Gallery</h1>
-
-    <div id="nanogallery2" data-nanogallery2='{
-            "thumbnailHeight": 210, "thumbnailWidth": 210,
-            "thumbnailL1GutterWidth": 90,
-            "thumbnailL1GutterHeight": 90,
-            "itemsBaseURL": "img/flc/",
-            "thumbnailStacks": 4,
-            "thumbnailStacksTranslateZ": 0.3,
-            "thumbnailStacksRotateX": 0.9,
-            "galleryBuildInit2": "perspective_900px|perspective-origin_50% 150%"
-          }'>
-        <a href="flcphoto1.jpg" data-ngthumb="flcphoto1tmb-min.jpg"></a>
-        <a href="flcphoto2.jpg" data-ngthumb="flcphoto2tmb-min.jpg"></a>
-        <a href="flcphoto3.jpg" data-ngthumb="flcphoto3tmb-min.jpg"></a>
-        <a href="flcphoto4.jpg" data-ngthumb="flcphoto4tmb-min.jpg"></a>
-        <a href="flcphoto5.jpg" data-ngthumb="flcphoto5tmb-min.jpg"></a>
-        <a href="flcphoto6.jpg" data-ngthumb="flcphoto6tmb-min.jpg"></a>
-        <a href="flcphoto7.jpg" data-ngthumb="flcphoto7tmb-min.jpg"></a>
-        <a href="flcphoto8.jpg" data-ngthumb="flcphoto8tmb-min.jpg"></a>
-        <a href="flcphoto9.jpg" data-ngthumb="flcphoto9tmb-min.jpg"></a>
-        <a href="flcphoto10.jpg" data-ngthumb="flcphoto10tmb-min.jpg"></a>
-        <a href="flcphoto11.jpg" data-ngthumb="flcphoto11tmb-min.jpg"></a>
-        <a href="flcphoto12.jpg" data-ngthumb="flcphoto12tmb-min.jpg"></a>
-    </div>`)
-}
